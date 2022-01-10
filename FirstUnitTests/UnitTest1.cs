ba6954841f5297a867d5f1625b16069150949d78
Nick <nsagias@gmail.com>
2022-01-10 01:57:42 -0500

    Create first unit test, passing and failing add example

--- FirstUnitTests/UnitTest1.cs
+++ FirstUnitTests/UnitTest1.cs
@@ -5,8 +5,16 @@ namespace FirstUnitTests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
-    {
+    public void PassingTest() {
+      Assert.Equal(4, Add(2,2));
+    }
+    
+    [Fact]
+    public void FailingTest() {
+      Assert.Equal(5, Add(2 ,2));
+    }
 
+    int Add(int x, int y) {
+      return x + y;
     }
 }
