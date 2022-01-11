1b7f2c3145f719134e55e6748824d807725a62a1
Nick <nsagias@gmail.com>
2022-01-10 22:44:21 -0500

    Add rename first test passing test, create add_two failing test

--- unit-testing-using-dotnet-test/AddService.Tests/AddService_AddTwoShould.cs
+++ unit-testing-using-dotnet-test/AddService.Tests/AddService_AddTwoShould.cs
@@ -7,11 +7,23 @@ namespace Add.UnitTests.Services;
 public class AddService_AddTwoShould
 {
     [Fact]
-    public void AddTwo() {
+    public void Add_Two_Passing_Test() {
       var addService = new AddService();
+
       double result = addService.Add(1, 2);
       double expectedOutput = 3;
       
       Assert.Equal(result, expectedOutput);
     }
+
+    [Fact]
+    public void Add_Two_Failing_test() {
+      var addService = new AddService();
+
+      double result = addService.Add(2,2);
+      double expectedOutput = 3;
+
+      Assert.NotEqual(result, expectedOutput);
+    }
+
 }
