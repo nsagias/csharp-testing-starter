00b569330b2dcbf1f41873ea6b6f043d63531b01
Nick <nsagias@gmail.com>
2022-01-10 22:49:33 -0500

    Refactor addService

--- unit-testing-using-dotnet-test/AddService.Tests/AddService_AddTwoShould.cs
+++ unit-testing-using-dotnet-test/AddService.Tests/AddService_AddTwoShould.cs
@@ -4,13 +4,21 @@ using Add.Service;
 
 namespace Add.UnitTests.Services;
 
-public class AddService_AddTwoShould
-{
+public class AddService_AddTwoShould {
+
+    public readonly AddService _addService;
+    
+    public AddService_AddTwoShould() {
+      
+      _addService = new AddService();
+      
+    }
+
     [Fact]
     public void Add_Two_Passing_Test() {
-      var addService = new AddService();
+      // var addService = new AddService();
 
-      double result = addService.Add(1, 2);
+      double result = _addService.Add(1, 2);
       double expectedOutput = 3;
       
       Assert.Equal(result, expectedOutput);
@@ -18,9 +26,9 @@ public class AddService_AddTwoShould
 
     [Fact]
     public void Add_Two_Failing_test() {
-      var addService = new AddService();
+      // var addService = new AddService();
 
-      double result = addService.Add(2,2);
+      double result = _addService.Add(2,2);
       double expectedOutput = 3;
 
       Assert.NotEqual(result, expectedOutput);
