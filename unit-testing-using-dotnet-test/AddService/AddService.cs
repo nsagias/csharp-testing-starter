253d95effc640adf6c38df6cbc0980b004ce3ee9
Nick <nsagias@gmail.com>
2022-01-10 22:40:05 -0500

    Write code to pass test

--- unit-testing-using-dotnet-test/AddService/AddService.cs
+++ unit-testing-using-dotnet-test/AddService/AddService.cs
@@ -3,6 +3,9 @@
 namespace Add.Service;
 public class AddService {
   public double Add(int num1, int num2) {
+    
+    return num1 + num2;
+    
     throw new NotImplementedException("Add is not implmented");
   }
 }
